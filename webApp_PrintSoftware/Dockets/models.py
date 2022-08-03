03de60dcc767226e30db6aab6fd317baa5abdebb
Noah Jina <109239448+noahj9@users.noreply.github.com>
2022-08-03 13:32:25 -0600

    development branch

--- webApp_PrintSoftware/Dockets/models.py
+++ webApp_PrintSoftware/Dockets/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Docket(models.Model):
+    number = models.TextField(max_length=100)
+    client = models.TextField()
+    rep = models.TextField()
+    content = models.TextField()
+    date_created = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #auto deletes the docket if the author is deleted
