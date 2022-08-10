003fbad27dfed16b0f9041e5b9660786e39eec77
Noah Jina <109239448+noahj9@users.noreply.github.com>
2022-08-09 20:22:47 -0600

    bunch of stuff
    added clients to DB
    updated admin portal database
    revamped form and backend of form
    started form frontend
    added import/export functionality for database
    updated models

--- webApp_PrintSoftware/Dockets/admin.py
+++ webApp_PrintSoftware/Dockets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import clients
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+@admin.register(clients)
+class clientData(ImportExportModelAdmin):
+    pass
