ecfab8bc281b0ab3ff405338ebc8d4eb667369a5
Noah Jina <109239448+noahj9@users.noreply.github.com>
2022-10-02 18:55:19 -0400

    fixed models, dropdowns
    form fully functional, add contact email and phone display by felix

--- webApp_PrintSoftware/Dockets/forms.py
+++ webApp_PrintSoftware/Dockets/forms.py
@@ -8,6 +8,7 @@ from django_addanother.widgets import AddAnotherWidgetWrapper
 
 # Create form for docket creation
 class NewDocketForm(ModelForm): #model form for new docket
+
     class Meta:
         model = Docket #the model it follows
         fields = ('customer_name', 'date', 'date_required', 'contact',  #the fields that are part of the form
@@ -21,21 +22,21 @@ class NewDocketForm(ModelForm): #model form for new docket
             'date_required': DatePickerInput(attrs={'class': 'form-control'}),
             'contact': AddAnotherWidgetWrapper(forms.Select(attrs={'class':'form-control'}), reverse_lazy('contact-create')),
             'account': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Account'}),
-            'terms': forms.TextInput(attrs={'class':'form-control'}),
+            'terms': forms.Select(attrs={'class':'form-control'}),
             'customer_PO': forms.TextInput(attrs={'class':'form-control'}),
-            'deposit': forms.TextInput(attrs={'class':'form-control'}),
+            'deposit': forms.Select(attrs={'class':'form-control'}),
             'rep': forms.Select(attrs={'class':'form-control'}),
             'csr': forms.Select(attrs={'class':'form-control'}),
             'quantity_1': forms.TextInput(attrs={'class':'form-control'}),
             'description_1': forms.TextInput(attrs={'class':'form-control'}),
             'finished_size_1': forms.TextInput(attrs={'class':'form-control'}),
-            'stock_1': forms.TextInput(attrs={'class':'form-control'}),
+            'stock_1': forms.Select(attrs={'class':'form-control'}),
             'machine_1': forms.CheckboxSelectMultiple(attrs={'class':'form-check form-check-inline ml-4'}),
             'run_quantity_1': forms.TextInput(attrs={'class':'form-control'}),
             'sheet_size_1': forms.TextInput(attrs={'class':'form-control'}),
             'run_size_1': forms.TextInput(attrs={'class':'form-control'}),
-            'proof_1': forms.TextInput(attrs={'class':'form-control'}),
-            'inks_1': forms.TextInput(attrs={'class':'form-control'}),
+            'proof_1': forms.Select(attrs={'class':'form-control'}),
+            'inks_1': forms.Select(attrs={'class':'form-control'}),
             'instructions_1': forms.TextInput(attrs={'class':'form-control'}),
             'bindery_1': forms.TextInput(attrs={'class':'form-control'}),
             'file_1': forms.TextInput(attrs={'class':'form-control'}),
