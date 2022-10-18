d2266e7cfffa905b8ebb7e8a94e2e2f233c82df8
Noah Jina <109239448+noahj9@users.noreply.github.com>
2022-10-18 18:39:37 -0400

    more form stuff
    almost done

--- webApp_PrintSoftware/Dockets/forms.py
+++ webApp_PrintSoftware/Dockets/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Docket, Stock
+from .models import Docket, Stock, Proof, Ink
 from .widgets import DatePickerInput
 from django.urls import reverse_lazy
 from django_addanother.widgets import AddAnotherWidgetWrapper
@@ -8,7 +8,34 @@ from django_addanother.widgets import AddAnotherWidgetWrapper
 
 # Create form for docket creation
 class NewDocketForm(ModelForm): #model form for new docket
+    machines = ((1, 'Xerox 1000'),
+            (2, 'Xerox Nuvera'),
+            (3, 'Xerox Versant'),
+            (4, 'Roland'),
+            (5, '9810'),
+            (6, 'DI'),
+            (7, 'Komori'),
+            (8, 'Outsource'),
+            (9, 'Design'),
+            (10, 'Shipping'),
+            (11, 'Mailing'),
+            (12, 'Postage'),
+            (13, 'PLS'),
+            (14, 'Other'),
+            (15, 'N/A'))
+
     stock_1 = forms.ModelChoiceField(queryset=Stock.objects.all(), widget=forms.Select(attrs = {'class': 'form-control'}))
+    stock_2 = forms.ModelChoiceField(queryset=Stock.objects.all(), widget=forms.Select(attrs = {'class': 'form-control'}))
+    stock_3 = forms.ModelChoiceField(queryset=Stock.objects.all(), widget=forms.Select(attrs = {'class': 'form-control'}))
+    proof_1 = forms.ModelChoiceField(queryset=Proof.objects.all(), widget=forms.Select(attrs = {'class': 'form-control'}))
+    proof_2 = forms.ModelChoiceField(queryset=Proof.objects.all(), widget=forms.Select(attrs = {'class': 'form-control'}))
+    proof_3 = forms.ModelChoiceField(queryset=Proof.objects.all(), widget=forms.Select(attrs = {'class': 'form-control'}))
+    inks_1 = forms.ModelChoiceField(queryset=Ink.objects.all(), widget=forms.Select(attrs = {'class': 'form-control'}))
+    inks_2 = forms.ModelChoiceField(queryset=Ink.objects.all(), widget=forms.Select(attrs = {'class': 'form-control'}))
+    inks_3 = forms.ModelChoiceField(queryset=Ink.objects.all(), widget=forms.Select(attrs = {'class': 'form-control'}))
+    machine_1 =forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,choices = machines)
+    machine_2 =forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,choices = machines)
+    machine_3 =forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs = {'class': 'list-inline'}),choices = machines)
 
     class Meta:
         model = Docket #the model it follows
@@ -17,7 +44,7 @@ class NewDocketForm(ModelForm): #model form for new docket
                     'deposit', 'rep', 'csr', 'reception_notes',
                     'quantity_1', 'description_1', 'finished_size_1', 'stock_1', 'machine_1', 'run_quantity_1', 'sheet_size_1', 'run_size_1', 'proof_1', 'inks_1', 'instructions_1', 'bindery_1', 'file_1', 'price_comission_1', 'shipping_1',
                     'quantity_2', 'description_2', 'finished_size_2', 'stock_2', 'machine_2', 'run_quantity_2', 'sheet_size_2', 'run_size_2', 'proof_2', 'inks_2', 'instructions_2', 'bindery_2', 'file_2', 'price_comission_2', 'shipping_2',
-                    'quantity_3', 'description_3', 'finished_size_3', 'stock_3', 'machine_3', 'run_quantity_3', 'sheet_size_3', 'run_size_3', 'proof_3', 'inks_3', 'instructions_3', 'bindery_3', 'file_3', 'price_comission_3', 'shipping_3'
+                    'quantity_3', 'description_3', 'finished_size_3', 'stock_3', 'machine_3', 'run_quantity_3', 'sheet_size_3', 'run_size_3', 'proof_3', 'inks_3', 'instructions_3', 'bindery_3', 'file_3', 'price_comission_3', 'shipping_3', 'reception_notes'
                 )
 
         widgets = { #allows utilization of bootstrap form controls and styling
@@ -34,12 +61,9 @@ class NewDocketForm(ModelForm): #model form for new docket
             'quantity_1': forms.TextInput(attrs={'class':'form-control'}),
             'description_1': forms.TextInput(attrs={'class':'form-control'}),
             'finished_size_1': forms.TextInput(attrs={'class':'form-control'}),
-            'machine_1': forms.CheckboxSelectMultiple(attrs={'class':'form-check form-check-inline ml-4'}),
             'run_quantity_1': forms.TextInput(attrs={'class':'form-control'}),
             'sheet_size_1': forms.TextInput(attrs={'class':'form-control'}),
             'run_size_1': forms.TextInput(attrs={'class':'form-control'}),
-            'proof_1': forms.Select(attrs={'class':'form-control'}),
-            'inks_1': forms.Select(attrs={'class':'form-control'}),
             'instructions_1': forms.TextInput(attrs={'class':'form-control'}),
             'bindery_1': forms.TextInput(attrs={'class':'form-control'}),
             'file_1': forms.TextInput(attrs={'class':'form-control'}),
@@ -48,13 +72,10 @@ class NewDocketForm(ModelForm): #model form for new docket
             'quantity_2': forms.TextInput(attrs={'class':'form-control'}),
             'description_2': forms.TextInput(attrs={'class':'form-control'}),
             'finished_size_2': forms.TextInput(attrs={'class':'form-control'}),
-            'stock_2': forms.Select(attrs={'class':'form-control'}),
             'machine_2': forms.CheckboxSelectMultiple(attrs={'class':'form-check form-check-inline ml-4'}),
             'run_quantity_2': forms.TextInput(attrs={'class':'form-control'}),
             'sheet_size_2': forms.TextInput(attrs={'class':'form-control'}),
             'run_size_2': forms.TextInput(attrs={'class':'form-control'}),
-            'proof_2': forms.Select(attrs={'class':'form-control'}),
-            'inks_2': forms.Select(attrs={'class':'form-control'}),
             'instructions_2': forms.TextInput(attrs={'class':'form-control'}),
             'bindery_2': forms.TextInput(attrs={'class':'form-control'}),
             'file_2': forms.TextInput(attrs={'class':'form-control'}),
@@ -63,16 +84,14 @@ class NewDocketForm(ModelForm): #model form for new docket
             'quantity_3': forms.TextInput(attrs={'class':'form-control'}),
             'description_3': forms.TextInput(attrs={'class':'form-control'}),
             'finished_size_3': forms.TextInput(attrs={'class':'form-control'}),
-            'stock_3': forms.Select(attrs={'class':'form-control'}),
             'machine_3': forms.CheckboxSelectMultiple(attrs={'class':'form-check form-check-inline ml-4'}),
             'run_quantity_3': forms.TextInput(attrs={'class':'form-control'}),
             'sheet_size_3': forms.TextInput(attrs={'class':'form-control'}),
             'run_size_3': forms.TextInput(attrs={'class':'form-control'}),
-            'proof_3': forms.Select(attrs={'class':'form-control'}),
-            'inks_3': forms.Select(attrs={'class':'form-control'}),
             'instructions_3': forms.TextInput(attrs={'class':'form-control'}),
             'bindery_3': forms.TextInput(attrs={'class':'form-control'}),
             'file_3': forms.TextInput(attrs={'class':'form-control'}),
             'price_comission_3': forms.TextInput(attrs={'class':'form-control'}),
             'shipping_3': forms.TextInput(attrs={'class':'form-control'}),
+            'reception_notes': forms.TextInput(attrs={'class':'form-control'}),
         }
