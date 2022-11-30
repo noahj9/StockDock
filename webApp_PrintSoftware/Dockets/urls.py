f2077e71e4eeea130ff0ecb6c8393a71a581cde3
noahj9 <109239448+noahj9@users.noreply.github.com>
2022-11-30 17:36:11 -0500

    Update urls.py

--- webApp_PrintSoftware/Dockets/urls.py
+++ webApp_PrintSoftware/Dockets/urls.py
@@ -3,16 +3,16 @@ from . import views
 from .views import ContactCreate, CreateDocket, ClientCreate
 
 urlpatterns = [
-#     path('', views.home, name='dockets-home'),
-#     path('newDocket/', CreateDocket.as_view(), name='dockets-new'),
-#     path('updateDocket/<str:pk>/', views.updateDocket, name='dockets-update'),
-#     path('deleteDocket/<str:pk>/', views.deleteDocket, name='dockets-delete'),
-#     path('printDocket/<str:pk>/', views.printDocket, name='dockets-print'),
-#     re_path(r'^create-contact/$', ContactCreate.as_view(), name='contact-create'),
-#     re_path(r'^create-client/$', ClientCreate.as_view(), name='client-create'),
-#     path('cloneDocket/<str:pk>/', views.cloneDocket, name='dockets-clone'),
-#     path('addJob/<str:pk>/', views.addJob, name='dockets-addJob'),
-#     path('ajax/update_subCat/', views.updateSubCats, name='get_contactInfo_ajax'),
-#     path('ajax/updateContacts', views.getContacts, name = 'get-contacts'),
+    path('', views.home, name='dockets-home'),
+    path('newDocket/', CreateDocket.as_view(), name='dockets-new'),
+    path('updateDocket/<str:pk>/', views.updateDocket, name='dockets-update'),
+    path('deleteDocket/<str:pk>/', views.deleteDocket, name='dockets-delete'),
+    path('printDocket/<str:pk>/', views.printDocket, name='dockets-print'),
+    re_path(r'^create-contact/$', ContactCreate.as_view(), name='contact-create'),
+    re_path(r'^create-client/$', ClientCreate.as_view(), name='client-create'),
+    path('cloneDocket/<str:pk>/', views.cloneDocket, name='dockets-clone'),
+    path('addJob/<str:pk>/', views.addJob, name='dockets-addJob'),
+    path('ajax/update_subCat/', views.updateSubCats, name='get_contactInfo_ajax'),
+    path('ajax/updateContacts', views.getContacts, name = 'get-contacts'),
 ]
 
