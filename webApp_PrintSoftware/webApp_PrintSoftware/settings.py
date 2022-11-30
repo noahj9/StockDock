24cdb57ee9beb8576580e9872792be8efa84f200
noahj9 <109239448+noahj9@users.noreply.github.com>
2022-11-30 18:00:47 -0500

    Update settings.py

--- webApp_PrintSoftware/webApp_PrintSoftware/settings.py
+++ webApp_PrintSoftware/webApp_PrintSoftware/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(","), "stockdock-app-5kkmb.ondigitalocean.app"]
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 #DEVELOPMENT_MODE = True
 
