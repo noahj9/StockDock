2e44a8913f569f04ca0765beaf1e5aa3227f4e01
noahj9 <109239448+noahj9@users.noreply.github.com>
2022-11-30 18:28:59 -0500

    Update settings.py

--- webApp_PrintSoftware/webApp_PrintSoftware/settings.py
+++ webApp_PrintSoftware/webApp_PrintSoftware/settings.py
@@ -69,7 +69,9 @@ ROOT_URLCONF = 'webApp_PrintSoftware.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+        os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
